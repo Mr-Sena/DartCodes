3e4ef735d854d7aea180afa0a1aa8c8aca9e4903
Mr-Sena <jsenna985@gmail.com>
2021-08-03 17:55:48 -0300

    Um comentário sobreo tipagem dinâmica
    Pelo visto o tipo dinâmica é uma evolução presente somente no dados
    fracamente tipado.

--- Washington-Developer/Workspace/atributes/variaveis.dart
+++ Washington-Developer/Workspace/atributes/variaveis.dart
@@ -18,6 +18,9 @@ void main () {
   //Faz a definição do tipo do dado explicitamente, no caso, que não poderá ser alterada.
   bool fortementeTipadaEstatico = true;
 
+  //Pelo visto o tipo dinâmica é uma evolução presente somente no dados fracamente tipado.
+
+
 
   //Os tipos, normalmente são podem receber o valor nullo,
   //para permitir essa atribuição, adicione ? depois do tipo
